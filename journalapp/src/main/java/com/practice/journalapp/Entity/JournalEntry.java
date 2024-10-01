d6cb3d838a999bff88e12e0498ce5b52e1bcaaeb
Emon <emon@kaz-software.com>
2024-10-02 00:20:28 +0600

    Removed boilerplate getter setter code and added lombok annotation.

--- journalapp/src/main/java/com/practice/journalapp/Entity/JournalEntry.java
+++ journalapp/src/main/java/com/practice/journalapp/Entity/JournalEntry.java
@@ -1,5 +1,7 @@
 package com.practice.journalapp.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,42 +9,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Getter
+@Setter
 public class JournalEntry {
     @Id // This field will be the unique for the collection
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime dateTime;
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
